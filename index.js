1369e43ee6dbaccf3b2f7d59880bf842af56811e
Pedro Gonzalez <orion@Pedros-MacBook-Pro.local>
2024-03-23 13:14:09 -0500

    adding arrow callback function for method seEList.

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ let eList = []
 let dList = []
 let rList = []
 
+connection.connect((err) =>{
+    if (err) {throw(err)}
+    setEList()
+    setDList()
+    //setRList()
+    whatToDo()
+})
+
+const setEList = () => {
+eList = []
+connection.query(
+  `SELECT CONCAT(first_name, ' ', last_name) AS name FROM employee`,
+  (err,data) => { if (err) { throw(err) }
+  data.forEach(name =>{
+    eList.push(`${name.name}`)
+  })
+  eList.push('None')
+})
+}
+
